7cc5d4223b9105e52f6c969b050242ba50cc281e
Sagor Rana <sagorranait@gmail.com>
2022-11-10 12:21:40 +0600

    JWT functionality is added.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Sangita's Kitchen (Classic Food)
+
+This server app has all the API functionality for Sangita's Kitchen (Classic Food). Please have look at the below : 
+
+## Services
+ - All Services API : 
+ - Single Service API : 
+ - Single Service API demo : 
+
+ ## Reviews
+ - All Reviews API : 
+ - Single Review API : 
+ - Single Review API demo : 
--- index.js
+++ index.js
@@ -15,9 +15,33 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.SERVER_USER}:${process.env.SERVER_PASSWORD}@cluster0.b18cp.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const verifyJWToken = (req, res, next) => {
+   const authHeaderToken = req.headers.authorization;
+
+   if(!authHeaderToken){
+       return res.status(401).send({message: 'Un-Authorized Entry !!!'});
+   }
+   const token = authHeaderToken.split(' ')[1];
+
+   jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+       if(err){
+           return res.status(403).send({message: 'Illegal Access !!!'});
+       }
+       req.decoded = decoded;
+       next();
+   })
+}
+
 const runServer = async () => {
    const services = client.db('sangitasKitchen').collection('services');
    const reviews = client.db('sangitasKitchen').collection('reviews');
+
+   // JWT
+   app.post('/jwt', (req, res) =>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1d'})
+      res.send({token})
+  });
 
    // Services Route
    app.get('/services', async (req, res) => {
@@ -48,7 +72,13 @@ const runServer = async () => {
    });
 
    // Review Route
-   app.get('/review', async (req, res) => {
+   app.get('/review', verifyJWToken, async (req, res) => {
+      const decodedToken = req.decoded;
+            
+      if(decodedToken.email !== req.query.email){
+            res.status(403).send({message: 'unauthorized access'})
+      }
+
       let query = {};
       if (req.query.email) {
           query = {
@@ -69,7 +99,7 @@ const runServer = async () => {
   app.get('/review/:serviceId', async (req, res) => {
       const id = req.params.serviceId;
       let query = { "service.sid": id };
-      const cursor = reviews.find(query);
+      const cursor = reviews.find(query).sort({date: -1});
       const serviceReview = await cursor.toArray();
       res.send(serviceReview);
    });
